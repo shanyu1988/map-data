95c90ff6563fe7bcca01c186b94c17daeb1b7d7f
ShanYuPC_01 <359987011@qq.com>
2020-09-01 22:50:14 +0800

    create

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@/assets/common.scss";
+Vue.config.productionTip = false;
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="main">
+  <h1>赣榆区站点分布</h1>
+  <gan-yu></gan-yu>
+</div>
+</template>
+
+<script>
+  import GanYu from "@/components/map/map"
+export default {
+  name: "Home",
+  components: {
+    GanYu
+  }
+};
+</script>
+<style lang="scss" scoped>
+  .main{
+    width: 100%;
+    height: 100%;
+    background-color: #1c1c1c;
+  }
+</style>
